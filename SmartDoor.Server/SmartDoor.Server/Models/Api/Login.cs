06e0aa8609afa9490b64667d081b5ec4cf9eb9cb
ondrej-danis <79908351+danis-on@users.noreply.github.com>
2021-12-20 23:26:54 +0100

    App first init

--- SmartDoor.Server/SmartDoor.Server/Models/Api/Login.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace SmartDoor.Server.Models.Api
-{
-    public record class LoginRequest (string login, string password, long? validityInSeconds);
-    public record class LoginResponse (string token);
-}
